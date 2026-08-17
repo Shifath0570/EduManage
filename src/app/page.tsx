b82e0c9143240295fe05b6d2c63a052e997b6e4e
ShifathPro057 <sharifulamin2023@gmail.com>
2026-08-17 23:20:14 +0600

    new components update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import Hero from "./component/Hero";
 import StatisticsSection from "./component/StatisticsSection";
 import QuestionsSection from "./component/QuestionsSection";
+import WhyChooseUs from "./component/WhyChooseUs";
+import Testimonials from "./component/Testimonials";
+import Notice from "./component/Notice";
+
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <StatisticsSection />
+      <Notice />
+      <WhyChooseUs/>
+      <Testimonials/>
       <QuestionsSection />
     </div>
   );
