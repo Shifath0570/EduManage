ce0be902f2a2c73023b54ed362377dde7fc00cc3
MaksumulEmon <maksumulemon300@gmail.com>
2026-08-17 18:01:41 +0600

    About section Add by Emon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import AboutSchool from "./component/AboutSchool";
+
+
 
 export default function Home() {
   return (
     <div>
-      
+      <AboutSchool/>
     </div>
   );
 }
