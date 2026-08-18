448dc16383b0873c2323656465edb1ec70746414
ShifathPro057 <sharifulamin2023@gmail.com>
2026-08-18 13:29:47 +0600

    new components about and feature update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,16 @@ import QuestionsSection from "./component/QuestionsSection";
 import WhyChooseUs from "./component/WhyChooseUs";
 import Testimonials from "./component/Testimonials";
 import Notice from "./component/Notice";
+import FeaturedSection from "./component/FeaturedSection";
+import AboutSchool from "./component/AboutSchool";
 
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <AboutSchool />
+      <FeaturedSection />
       <StatisticsSection />
       <Notice />
       <WhyChooseUs/>
