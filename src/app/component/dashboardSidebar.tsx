df56ab09cdedec56e655feb32cb77697f992c11d
MaksumulEmon <maksumulemon300@gmail.com>
2026-08-24 21:59:02 +0600

    Implement: Teacher Dashboard Enter Mark Icone Change

--- src/app/component/dashboardSidebar.tsx
+++ src/app/component/dashboardSidebar.tsx
@@ -18,6 +18,7 @@ import {
   LogOut,
   Shield,
   SquarePlus,
+  PencilLine,
 } from "lucide-react";
 
 import { useSession, signOut } from "../lib/auth-client";
@@ -38,7 +39,7 @@ const teacherNavItems: NavItem[] = [
   { id: "viewAttendance", href: "/teacher/viewAttendance", label: "View Attendance", icon: <User className="w-5 h-5" /> },
   { id: "createAssingment", href: "/teacher/createAssingment", label: "Create Assingment", icon: <Bookmark className="w-5 h-5" /> },
   { id: "createExam", href: "/teacher/createExam", label: "Create Exam", icon: <SquarePlus className="w-5 h-5" /> },  
-  { id: "enterMarks", href: "/teacher/enterMarks", label: "Enter Marks", icon: <Bookmark className="w-5 h-5" /> },
+  { id: "enterMarks", href: "/teacher/enterMarks", label: "Enter Marks", icon: <PencilLine className="w-5 h-5" /> }, 
   { id: "viewResuls", href: "/teacher/viewResuls", label: "View Resuls", icon: <Bookmark className="w-5 h-5" /> },
   { id: "createNotice", href: "/teacher/createNotice", label: "Create Notice", icon: <Bookmark className="w-5 h-5" /> },
   { id: "viewNotice", href: "/teacher/viewNotice", label: "View Notice", icon: <Bookmark className="w-5 h-5" /> },
