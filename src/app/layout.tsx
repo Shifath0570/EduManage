a12bcaf5615ec617c173e2a1b785d2c2b8851a02
Osman Goni <osman.mirpur55@gmail.com>
2026-09-05 11:32:27 +0600

    Added react-hot-toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./component/Footer";
 import Navbar from "./component/Navbar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
         </main>
         <Footer></Footer>
+        <Toaster></Toaster>
       </body>
     </html>
   );
