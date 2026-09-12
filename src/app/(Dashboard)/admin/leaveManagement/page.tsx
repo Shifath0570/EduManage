09b5c3d02fa988eb3f86253b7deb1fa6800342b4
ShifathPro057 <sharifulamin2023@gmail.com>
2026-09-12 22:54:43 +0600

    leave management page create

---
+++ src/app/(Dashboard)/admin/leaveManagement/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const leaveManagement = () => {
+    return (
+        <div>
+            <h2>Leave Management</h2>
+        </div>
+    );
+};
+
+export default leaveManagement;
--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -308,3 +308,8 @@ export async function POST(req: NextRequest) {
         );
     }
 }
+
+
+
+
+
