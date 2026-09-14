9a4cdaee7b08fddd1b4de07f9e8a6ed791e4935a
ShifathPro057 <sharifulamin2023@gmail.com>
2026-09-14 11:20:04 +0600

    new item create

--- src/app/(Dashboard)/admin/leaveManagement/page.tsx
+++ src/app/(Dashboard)/admin/leaveManagement/page.tsx
@@ -9,3 +9,4 @@ const leaveManagement = () => {
 };
 
 export default leaveManagement;
+
---
+++ src/proxy.tsx
@@ -0,0 +1,43 @@
+import { headers } from 'next/headers'
+import { NextResponse, type NextRequest } from 'next/server'
+import { auth } from './app/lib/auth'
+
+export async function proxy(request: NextRequest) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (!session) {
+    return NextResponse.redirect(new URL('/auth/Login', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    '/admin/createStudent/student',
+    '/admin/createStudent',
+    '/admin/createTeacher/teacher',
+    '/admin/createTeacher',
+    '/admin/manageTeachers',
+    '/admin/manageStudents',
+    '/admin/viewAttendance',
+    '/admin/createExam',
+    '/admin/allExams',
+    '/admin/viewNotice',
+    '/admin/manageBlogs',
+    '/admin/contactMessages',
+    '/admin/feeCollection',
+    '/admin/teacherSalary',
+    '/teacher',
+    '/student',
+    '/teacher/takeAttendance',
+    '/teacher/viewAttendance',
+    '/teacher/createExam',
+    '/teacher/allExams',
+    '/teacher/enterMarks',
+    '/teacher/viewResuls',
+    '/teacher/teacherSalary',
+  ],
+}
